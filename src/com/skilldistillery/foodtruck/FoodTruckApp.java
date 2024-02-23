9a0485379a7d7f1c942eaa923d6d8d8fac183249
Bryan Bradley <bryrbradley@gmail.com>
2024-02-23 15:38:18 -0700

    Added readme and git ignore, setup repository

---
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruckApp {
+	private int maxTrucks = 5;
+	private FoodTruck[] trucks = new FoodTruck[maxTrucks];
+
+	public static void main(String[] args) {
+		FoodTruckApp fta = new FoodTruckApp();
+		fta.run();
+	}
+
+	public void run() {
+
+	}
+//	run() / init() / start()
+
+}
