32373ad56da336acb9981a44048969e31e399ccb
Bryan Bradley <bryrbradley@gmail.com>
2024-02-23 17:15:42 -0700

    V1 of FoodTruckProject and FoodTruckApp about 50% through the initial coding, create a ULM for the build

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -2,4 +2,53 @@ package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
 
+	private static int nextTruckId = 1;
+	private int tId = 0;
+	private String truckName = "";
+	private String foodType = "";
+	private double truckRating = 0.0;
+
+	public FoodTruck(String name, String food, double rating) {
+		this.tId = nextTruckId++;
+		this.truckName = name;
+		this.foodType = food;
+		this.truckRating = rating;
+	}
+
+	public int getId() {
+		return tId;
+	}
+
+	public void setiD(int ID) {
+		this.tId = ID;
+	}
+
+	public String getName() {
+		return truckName;
+	}
+
+	public void setName(String name) {
+		this.truckName = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return truckRating;
+	}
+
+	public void setRating(int rating) {
+		this.truckRating = rating;
+	}
+
+	public String toString() {
+		return "FoodTruck " + "ID: " + tId + "\n Truck Name: " + truckName + "\n FoodType: " + foodType
+				+ "\n Truck rating: " + truckRating;
+	}
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -1,17 +1,30 @@
 package com.skilldistillery.foodtruck;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
-	private int maxTrucks = 5;
-	private FoodTruck[] trucks = new FoodTruck[maxTrucks];
+	private static int maxTrucks = 5;
+	private static FoodTruck[] foodTrucks = new FoodTruck[maxTrucks];
+	private static int tCount = 0;
 
 	public static void main(String[] args) {
-		FoodTruckApp fta = new FoodTruckApp();
-		fta.run();
+		FoodTruckApp foodTApp = new FoodTruckApp();
+		foodTApp.run();
 	}
 
 	public void run() {
+		Scanner kb = new Scanner(System.in);
+		while (tCount > maxTrucks) {
+			System.out.println(" !!!! Welcome to the Food Truck App !!!!/n");
+			System.out.println("Please enter the name of the Food Truck: ");
+
+		}
+//		FoodTruck t1 = new FoodTruck();
+//		FoodTruck t2 = new FoodTruck();
+//		FoodTruck t3 = new FoodTruck();
+//		FoodTruck t4 = new FoodTruck();
+//		FoodTruck t5 = new FoodTruck();
 
 	}
-//	run() / init() / start()
 
 }
